034f7fa9ed4517bb10d8bac22f0b8a2dab8e69e2
anna hogue <annachogue@gmail.com>
2023-03-05 23:18:55 -0500

    set up routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const store = require('../db/store');
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+router.get('/notes', (req, res) => {
+    store
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
+  });
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.  
+router.post('/notes', (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
+
+  module.exports = router;
